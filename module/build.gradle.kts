cc5e9aa5d02d76a7bb4bc0b9f3cc35018a52855c
Troy <troyswanson1010@gmail.com>
2021-03-08 00:08:56 -0600

    gradle kotlindsl, groovy

---
+++ module/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    `kotlin-dsl`
+    `java-gradle-plugin`
+    id("maven-publish")
+}
+
+// # Define a plugin to build, give it an id
+gradlePlugin {
+    plugins {
+        create("keyForPlugin") {
+            id = "TMPlugin"
+            implementationClass = "uyt.DemoPlugin"
+        }
+    }
+}
+
+publishing {
+    publications {
+        create("pluginPublication", MavenPublication::class) {
+            from(project.components["java"])
+            groupId = "com.example.pg_pluginlibrary"
+            artifactId = project.name
+            version = "1.0.0"
+        }
+    }
+}
+
+dependencies {
+    implementation("com.android.tools.build:gradle:4.1.1")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
+    implementation(localGroovy())
+    implementation(gradleApi())
+}
