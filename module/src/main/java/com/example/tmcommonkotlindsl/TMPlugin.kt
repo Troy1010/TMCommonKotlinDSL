78274289a0a996240ab81cbf9402a9280791bd16
Troy <troyswanson1010@gmail.com>
2021-03-10 18:01:30 -0600

    easyPublishLocal, easyCleanPublishLocal

--- module/src/main/java/com/example/tmcommonkotlindsl/TMPlugin.kt
+++ module/src/main/java/com/example/tmcommonkotlindsl/TMPlugin.kt
@@ -7,7 +7,16 @@ import tmextensions.*
 open class TMPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         project.afterEvaluate {
-            tasks.registerOrderedPair("assemble", "publishToMavenLocal")
+            subprojects.forEach { it.tasks.registerOrderedPair("assemble", "publishToMavenLocal") }
+            tasks.register("easyCleanPublishLocal") {
+                group = "publishing"
+                dependsOn(project.getTasksByName("clean", true))
+                subprojects.forEach { finalizedBy(it.tasks.getByName("assemble_publishToMavenLocal")) }
+            }
+            tasks.register("easyPublishLocal") {
+                group = "publishing"
+                subprojects.forEach { finalizedBy(it.tasks.getByName("assemble_publishToMavenLocal")) }
+            }
         }
     }
 }
