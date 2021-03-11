052598ef6a8e5b4086f56ee201030ffa683f8d57
Troy <troyswanson1010@gmail.com>
2021-03-11 00:20:30 -0600

    clean_assemble pair

--- module/src/main/java/com/example/tmcommonkotlindsl/TMPlugin.kt
+++ module/src/main/java/com/example/tmcommonkotlindsl/TMPlugin.kt
@@ -7,6 +7,7 @@ import tmextensions.*
 open class TMPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         project.afterEvaluate {
+            tasks.tryRegisterOrderedPair("clean", "assemble")
             tasks.tryRegisterOrderedPair("assemble", "publishToMavenLocal")
             tasks.register("easyPublishLocal") {
                 group = "publishing"
